211a753f196bb50b0f7cea0b13facfebaed25440
ilovric00 <ilovric@profico.hr>
2021-02-08 22:55:02 +0100

    ESLint, Stylelint and Prettier configuration

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,19 +2,18 @@ import React from "react";
 
 import styles from "./Footer.module.scss";
 
-const Footer: React.FC = () => {
-  return (
-    <footer className={styles.container}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{" "}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-      </a>
-    </footer>
+const Footer: React.FC = () => (
+  <footer className={styles.container}>
+    <a
+      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by
+      {" "}
+      <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+    </a>
+  </footer>
   );
-};
 
 export default Footer;
