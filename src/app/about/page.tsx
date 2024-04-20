7fd71d7d77e07f45845da07f03d3462940fbd13a
Danijel Radomiljac <danijel.radomiljac@blankhq.co>
2024-04-20 22:29:04 +0200

    update metadata

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About',
+};
+
+const AboutPage = () => {
+  return <div>AboutPage</div>;
+};
+
+export default AboutPage;
---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Privacy',
+};
+
+const PrivacyPage = () => {
+  return <div>PrivacyPage</div>;
+};
+
+export default PrivacyPage;
